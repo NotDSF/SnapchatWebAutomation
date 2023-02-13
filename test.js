0d851ea8d08a503ac8b04a1079f6afdc869dc07d
NotDSF <67711164+NotDSF@users.noreply.github.com>
2023-02-13 20:58:56 +0000

    Fix awful indentation

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ const Snapchat = require(".");
 
 (async () => {
 	const client = await Snapchat.Login();
-    const chats = await client.GetChats();
+	const chats = await client.GetChats();
 	await client.OpenChat(chats[0]); // open first chat
 
-    // event only fires when a chat is open
+	// event only fires when a chat is open
 	client.events.on("message", async (message) => {
-        console.log(message);
+		console.log(message);
 		if (message.content === "ping") {
 			await client.SendMessage("pong!");
 		}
